52ede1e4d28b914c7cfb66a439a515cf1ed8812c
shun <kalbin2435@gmail.com>
2025-07-12 23:54:06 +0300

    colorscheme

--- init.lua
+++ init.lua
@@ -26,4 +26,4 @@ vim.g.mapleader = "\\"
 vim.g.maplocalleader = "\\"
 vim.api.nvim_set_keymap('n', '<C-u>', '<C-u>zz', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<C-d>', '<C-d>zz', { noremap = true, silent = true })
-
+vim.cmd("color retrobox")
